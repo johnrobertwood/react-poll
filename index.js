416e2e45181a0d269660852135e31bbe844c5730
John Wood <johnrobertwood@yahoo.com>
2016-06-20 20:41:33 -0700

    Adding react-routes

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Router, Route, Link, hashHistory } from 'react-router'
 
-const Hello = React.createClass({
+
+const Home = React.createClass({
   render() {
     return (
-      <h1>Hello {this.props.world}</h1>
+      <h1>Home</h1>
+    )
+  }
+})
+
+const About = React.createClass({
+  render() {
+    return (
+      <h1>About</h1>
     )
   }  
 })
 
-ReactDOM.render(
-  <Hello />,
-  document.getElementById('container')
-)
+const Contact = React.createClass({
+  render() {
+    return (
+      <h1>Contact</h1>
+    )
+  }
+})
+
+ReactDOM.render((
+  <Router history={hashHistory}>
+    <Route path="/" component={Home} />
+    <Route path="/about" component={About} />
+    <Route path="/contact" component={Contact} />
+  </Router>
+), document.getElementById('container'))
